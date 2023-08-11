d2d21b86242e5f7ae7e2bb81e8fb50a04f946e17
Irteza7 <m.irtezakhan@hotmail.com>
2023-08-11 18:05:07 +0200

    CORS

--- app/main.py
+++ app/main.py
@@ -2,13 +2,27 @@ from fastapi import FastAPI
 from app.database import engine
 from app.routers import post, user, auth, vote
 from app import models
+from fastapi.middleware.cors import CORSMiddleware
 
-
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+@app.get("/")
+async def hello():
+    return {"hello":"world"}
