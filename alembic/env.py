261a131f90c5b6119e9fcf3b3dd926926322c293
Irteza7 <m.irtezakhan@hotmail.com>
2023-08-12 14:38:03 +0200

    manually ran alembic cuz render free tier

--- alembic/env.py
+++ alembic/env.py
@@ -11,8 +11,8 @@ from app.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
-
+# config.set_main_option("sqlalchemy.url", f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
+config.set_main_option("sqlalchemy.url", settings.db_url)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
