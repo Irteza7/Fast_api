10bab1e348e2b8baa8dc847513128c795f27ea75
Irteza7 <m.irtezakhan@hotmail.com>
2023-08-14 00:02:06 +0200

    docker file and path change logic for render .env

--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
+import os
 from pydantic_settings import BaseSettings
+DEPLOY_ENV = os.getenv("DEPLOY_ENV")
 
 class Settings(BaseSettings):
     database_hostname : str 
@@ -12,7 +14,10 @@ class Settings(BaseSettings):
     db_url : str
 
     class Config:
-        env_file = "/etc/secrets/.env"
+        if DEPLOY_ENV == "RENDER":
+            env_file = "/etc/secrets/.env"
+        else:
+            env_file = "app/.env"
 
 
 settings = Settings()
